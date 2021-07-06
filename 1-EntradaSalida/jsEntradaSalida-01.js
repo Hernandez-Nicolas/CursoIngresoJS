c22766c1e7d466a404785a152a75cedd8c923619
Hernandez Nicolas <nicolash02@hotmail.com>
2021-07-05 23:19:07 -0300

    ejercicio completo

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Holasa Mundo");
+	alert("Esto funciona de maravilla");
 }
 
